28e1538c6e85b0c04bb40fd1b13e41fb6269b55f
BrittGall <crlnfllr@gmail.com>
2019-08-04 23:09:43 -0500

    made an array of the words for the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Listing the words/movies I want to be randomly gerated (hopefully this makes an array)
+const numberOfMovies = 11;
+let movies = new buildArray(numberOfMovies);
+movies[1] = "shrek";
+movies[2] = "sinbad";
+movies[3] = "prince of egypt";
+movies[4] = "antz";
+movies[5] = "spirit";
+movies[6] = "wallace & gromit";
+movies[7] = "flushed away";
+movies[8] = "kungfu panda";
+movies[9] = "rise of the guardians";
+movies[10] = "how to train your dragon";
+movies[11] = "home";
