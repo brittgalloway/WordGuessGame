746ee1a31061a74fd94a811d45e8fe75af703761
BrittGall <crlnfllr@gmail.com>
2019-08-05 00:26:27 -0500

    made an array of movies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,27 @@
 // Listing the words/movies I want to be randomly gerated (hopefully this makes an array)
 const numberOfMovies = 11;
-let movies = new buildArray(numberOfMovies);
-movies[1] = "shrek";
-movies[2] = "sinbad";
-movies[3] = "prince of egypt";
-movies[4] = "antz";
-movies[5] = "spirit";
-movies[6] = "wallace & gromit";
-movies[7] = "flushed away";
-movies[8] = "kungfu panda";
+let movies = [
+  "shrek",
+  "sinbad",
+  "prince of egypt",
+  "antz",
+  "spirit",
+  "wallace & gromit",
+  "flushed away",
+  "kungfu panda",
+  "rise of the guardians",
+  "how to train your dragon",
+  "home"
+];
+
 movies[9] = "rise of the guardians";
 movies[10] = "how to train your dragon";
 movies[11] = "home";
+
+function PickRandomWord(frm) {
+  // Generate a random number between 1 and NumberOfWords
+  var rnd = Math.ceil(Math.random() * NumberOfWords);
+
+  // Display the word inside the text box
+  frm.WordBox.value = words[rnd];
+}
