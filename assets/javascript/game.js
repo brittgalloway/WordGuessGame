c742bdc2606461db4e8fc0d4625e3a87528f3722
BrittGall <crlnfllr@gmail.com>
2019-08-06 00:51:14 -0500

    trying to create a span, and to give span _

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,35 +15,44 @@ const movies = [
 ];
 // words randomly choosen
 let computerMovie = movies[Math.floor(Math.random() * movies.length)];
+
+//Make guess into array for easy checking
+let movieChoiceArray = computerMovie.split("");
 // the array of blank spaces that will be filled will _ where movies letters would be
 let answerArray = [];
 for (let index = 0; index < computerMovie.length; index++) {
-  answerArray[index] = "_";
+  // Create new span
+  const letter = document.createElement("span");
+  // give span underscore as textContent
+  document.textContent = "_";
+  // give attribute to span that contains letter
+
+  // append span to some div
 }
 let remainingLetters = computerMovie.length;
-console.log(remainingLetters);
 // prints (_) on screen
 document.getElementById("blanks").innerHTML = answerArray;
 // game play
 // while (remainingLetters > 0) {
+// document.getElementById("blanks").innerHTML = answerArray.join("");
 // have player guess
+
 // show if guess is right or wrong
 // If if right, the letter appears in approperate spots
 // if wrong, the letter appears on the screen and "guesses remaining " decreases by 1
 // if guess get to 0 game over
-
 // }
 
 // ---------------------------
 // potentially discarded code
 
-// generates a random word from the movies array when any key is pushed
-// document.onkeyup = function(event) {
-//   if (event.key) {
-//     computerMovie = movies[Math.floor(Math.random() * movies.length)];
-//     console.log(computerMovie);
-//     document.getElementById("blanks").innerHTML = computerMovie;
-//     document.getElementById("score").innerHTML = remainingLetters;
-//   }
-// };
-// playable functions/code goes here
+// key is pushed, check it against the spans in the word
+document.onkeyup = function(event) {
+  const userKey = event.key;
+  const isLetterTrue = computerMovie.includes(userKey);
+  // if true letter appears
+  if (isLetterTrue) {
+  } else {
+    //your wrong, letter apears on screen, guesses remaining decreases by 1
+  }
+};
