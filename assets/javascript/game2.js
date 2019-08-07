278c3b9c1352ce4f86b5b8d8d2be33259bcb0d36
BrittGall <crlnfllr@gmail.com>
2019-08-07 14:12:50 -0500

    broke down (almost) line by line my intentions

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -15,8 +15,14 @@ const movies = [
 ];
 // game starts on key up
 document.onkeyup = function(event) {
+  // defining elements and things
+  const scoreElement = document.getElementById("score");
+  const guessElement = document.getElementById("wguess");
   const keypress = event.key;
+  let guessLeft = 5;
   if (keypress) {
+    document.textContent = guessLeft;
+    // game starts
     // words randomly choosen
     let computerMovie = movies[Math.floor(Math.random() * movies.length)];
     // console.log(computerMovie);
@@ -26,49 +32,57 @@ document.onkeyup = function(event) {
       answerArray[index] = "_";
       //   console.log(answerArray);
     }
+    // says how many (_) on screen based on given word
     let remainingLetters = computerMovie.length;
 
-    // prints (_) on screen
-    // console.log(answerArray);
+    // score variable
+    let score = 0;
+    // prints the mystery word
     document.getElementById("blanks").innerHTML = answerArray;
+    // if there are more than 0 letters to guess per word/title
     if (remainingLetters > 0) {
-      // user guess
+      // user guess on keyup
       let userGuess = keypress;
-      const scoreElement = document.getElementById("score");
-      const guessElement = document.getElementById("wguess");
-      let score = 0;
-      let guess = 5;
-
+      // loop for choosen word
       for (var key = 0; key < computerMovie.length; key++) {
+        // if user choice is correct
         if (computerMovie[key] === userGuess) {
-          answerArray[key] = userGuess;
+          answerArray[key] == userGuess;
           remainingLetters--;
           // Show the player their progress
-          console.log(answerArray);
+          // console.log(answerArray);
           document.getElementById("blanks").innerHTML = answerArray.join(" ");
           //   if the guess is wrong
         } else if (computerMovie[key] != userGuess) {
-          guess--;
-          guessElement.textContent = "Guesses " + guess;
-          console.log(guess);
-          //   if you run out of guesses and lose
-        } else if (guess == 0) {
-          guessElement.innerHTML = "Guesses " + guess;
-          getElementById("lose").innerHTML =
-            "Sorry, you lose. Want to try another movie title?";
-          //   if you run out of letters to guess(you fill all the spaces and win)
-        } else if (remainingLetters == 0) {
-          score++;
-
-          document.getElementById("blanks").innerHTML = answerArray.join("");
-          scoreElement.innerHTML = "Score " + score;
-          document.getElementById("win").innerHTML =
-            "You solved it! Can you beat them all?";
-          console.log(score);
-          guess = 0;
-        } else if (score == 11) {
-          document.getElementById("win").innerHTML = "YOU BEAT THEM ALL!";
+          // the blank stays?
+          answerArray[key] == computerMovie[key];
+          // guesses remaining (started with 5) goes down by 1
+          guessLeft--;
+          // Should print Guess 4 (if this is the 1st incorrect guess)
+          guessElement.textContent = "Guesses " + guessLeft;
+          console.log(guessLeft);
         }
+      }
+      //   if you run out of guesses and lose
+      if (guessLeft == 0) {
+        guessElement.innerHTML = "Guesses " + guessLeft;
+        // prints message for loss
+        document.getElementById("lose").innerHTML =
+          "Sorry, you lose. Want to try another movie title?";
+        //   if you run out of letters to guess(you fill all the spaces and win)
+      } else if (remainingLetters == 0 && guessLeft > 0) {
+        // increase score by 1 (for 1 full word spelled)
+        score++;
+        // prints score to screen
+        scoreElement.innerHTML = "Score " + score;
+        // congratulate player and encourge them to continue
+        document.getElementById("win").innerHTML =
+          "You solved it! Can you beat them all?";
+        console.log(score);
+        // if you beat all 11 words
+      } else if (score == 11) {
+        // print celebratory text
+        document.getElementById("win").innerHTML = "YOU BEAT THEM ALL!";
       }
     }
   }
