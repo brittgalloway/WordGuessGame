d1da2f1b23f2f22382819c6e0de7dbe193c9268f
BrittGall <crlnfllr@gmail.com>
2019-08-10 02:02:47 -0500

    on screen instructions disappear on Enter

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -13,90 +13,98 @@ const movies = [
   "how to train your dragon",
   "home"
 ];
-// total starting guesses
-let guessLeft = 5;
-// Declare random movie to be set on start
 let computerMovie = "";
 let answerArray = [];
 // game starts on key up
 document.onkeyup = function(event) {
   // defining elements and things
+  const blanksElement = document.getElementById("blanks");
   // define score element
   const scoreElement = document.getElementById("score");
   // define guess element
   const guessElement = document.getElementById("wguess");
   // define event key
   const keypress = event.key;
+  // Declare random movie to be set on start
 
+  // score variable
   if (keypress === "Enter") {
     // game starts
+
+    // // start instruction goes away
+    document.getElementById("start").style.visibility = "hidden";
+
     // words randomly choosen
     computerMovie = movies[Math.floor(Math.random() * movies.length)];
     console.log(computerMovie);
+
     // the array of blank spaces that will be filled will _ where movies letters would be
     for (let index = 0; index < computerMovie.length; index++) {
       answerArray[index] = "_";
       //   console.log(answerArray);
     }
+    let score = 0;
   }
   // if key pressed....
+
   if (keypress) {
-    // guessElement.textContent = guessLeft;
     // console.log(computerMovie);
+
     // says how many (_) on screen based on given word
     let remainingLetters = computerMovie.length;
-
-    // score variable
-    let score = 0;
+    let guessLeft = 5;
     // prints the mystery word
-    // console.log(answerArray);
+
     document.getElementById("blanks").textContent = answerArray.join(" ");
+    // Display starting amount of guesses
+    guessElement.textContent = "Remaining Gesses: " + guessLeft;
     // if there are more than 0 letters to guess per word/title
-    if (remainingLetters > 0) {
-      // user guess on keyup
-      let userGuess = keypress;
-      // loop for choosen word
-      for (var key = 0; key < remainingLetters; key++) {
-        // if user choice is correct
-        if (computerMovie[key] === userGuess) {
-          answerArray[key] = userGuess;
+    // if (remainingLetters > 0) {
+    // user guess on keyup
+    let userGuess = keypress;
+    // loop for choosen word
+    for (var key = 0; key < remainingLetters; key++) {
+      // if user choice is correct
+      guessLeft = 5;
+      if (computerMovie[key] === userGuess) {
+        answerArray[key] = userGuess;
 
-          // Show the player their progress
-          // console.log(answerArray);
-          document.getElementById("blanks").textContent = answerArray.join(" ");
-          //   if the guess is wrong
-        } else if (computerMovie[key] != userGuess) {
-          // guesses remaining (started with 5) goes down by 1
-          guessLeft - 1;
-          // Should print Guess 4 (if this is the 1st incorrect guess)
-          guessElement.textContent = "Guesses " + guessLeft;
-          // console.log(guessLeft);
-        }
+        // Show the player their progress
+
+        blanksElement.textContent = answerArray.join(" ");
+        guessElement.innerHTML = "Remaining Guesses: " + guessLeft;
+        //   if the guess is wrong
       }
-
-      //     //   if you run out of guesses and lose
-      //     if (guessLeft == 0) {
-      //       guessElement.innerHTML = "Guesses " + guessLeft;
-      //       // prints message for loss
-      //       document.getElementById("lose").innerHTML =
-      //         "Sorry, you lose. Want to try another movie title?";
-      //       //   if you run out of letters to guess(you fill all the spaces and win)
-      //     } else if (remainingLetters == 0 && guessLeft > 0) {
-      //       // increase score by 1 (for 1 full word spelled)
-      //       score++;
-      //       // prints score to screen
-      //       scoreElement.innerHTML = "Score " + score;
-
-      //       // congratulate player and encourge them to continue
-
-      //       document.getElementById("win").innerHTML =
-      //         "You solved it! Can you beat them all?";
-      //       console.log(score);
-      //       // if you beat all 11 words
-      //     } else if (score == 11) {
-      //       // print celebratory text
-      //       document.getElementById("win").innerHTML = "YOU BEAT THEM ALL!";
-      //     }
     }
+    if ([key] !== userGuess) {
+      //  guesses remaing goes down by 1
+      guessLeft -= 1;
+      // and prints
+      guessElement.innerHTML = "Remaining Guesses: " + guessLeft;
+    }
+    //  else if (remainingLetters == 0 && guessLeft > 0) {
+    //   // increase score by 1 (for 1 full word spelled)
+    //   score++;
+    //   // prints score to screen
+    //   scoreElement.textContent = "Score " + score;
+
+    //   // congratulate player and encourge them to continue
+
+    //   document.getElementById("win").textContent =
+    //     "You solved it! Can you beat them all?";
+    //   //       console.log(score);
+    //   // if you beat all 11 words
+    // } else score == 11;
+    // {
+    //   //       // print celebratory text
+    //   document.getElementById("win").textContent = "YOU BEAT THEM ALL!";
+    // }
+    // //   if you run out of guesses and lose
+    // else guessLeft == 0 {
+    //   guessElement.textContent = "Remaining Guesses: " + guessLeft;
+    //   // prints message for loss
+    //   document.getElementById("lose").innerHTML =
+    //     "Sorry, you lose. Want to try another movie title?";
+    //   if you run out of letters to guess(you fill all the spaces and win)
   }
 };
